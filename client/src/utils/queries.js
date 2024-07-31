38e9212ea98b2463600c8e152956ad77059e82e4
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-07-30 18:00:26 -0700

    setup of mutations and queries and auth for frontend using jwt

---
+++ client/src/utils/queries.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+query allUsers {
+    users {
+      _id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+query singleUser ($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+query me {
+    me {
+      _id
+      firstName
+      lastName
+    }
+  }
+`;
