411c99ddcb94f510f033cdd6b0af92529868bde9
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-09-16 17:39:15 -0700

    recolor and layout of homepage using bootstrap and css

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,24 +4,24 @@ import usThumbnail from '../assets/us_THUMBNAIL.jpg';
 
 const homePage = () => {
   return (
-    <div className="home">
-      <h1>Adrian & Tatiana</h1>
-      <div className="home-link">
-        <ul>
-          <li>
+    <div className="home container">
+      <h1 className="ms-auto my-4 text-white rounded">Adrian & Tatiana</h1>
+      <div className="home-link text-white p-1 rounded mx-auto">
+        <ul className="d-flex justifycontent-center list-unstyled">
+          <li className="mx-3">
             <Link to="/Story" >Share your Story</Link>
           </li>
-          <li>
+          <li className="mx-3">
             <Link to="/Gallery" >Gallery</Link>
           </li>
-          <li>
+          <li className="mx-3">
             <Link to='/RSVP' >RSVP</Link>
           </li>
         </ul>
       </div>
 
-      <div>
-        <p>Feburary 23, 2026 Phoenix, AZ</p>
+      <div className="mx-4">
+        <p className="lead">Feburary 23, 2026 Phoenix, AZ</p>
       </div>
     {/*short paragraph*/}
       <div>
@@ -30,7 +30,7 @@ const homePage = () => {
         </p>
       </div>
 
-      <div className="home-image">
+      <div className="home-image my-4">
         <img src={usThumbnail} alt="Adrian and Tatiana.." className="img"/>
       </div>
     </div>
