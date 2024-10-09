ef5dcbd430810a07186c9b21b9653af9eeee019d
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-10-08 17:39:03 -0700

    debugged rsvp form just needed the return for the form to work

--- client/src/pages/RSVP.jsx
+++ client/src/pages/RSVP.jsx
@@ -16,98 +16,118 @@ const Rsvp = () => {
     address: Yup.string().required("Address is required"),
     city: Yup.string().required("City is required"),
     state: Yup.string().required("State is required"),
-    zip: Yup.number().required("Zip Code is required")
+    zip: Yup.number().required("Zip Code is required"),
   });
 
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        email: "",
-        attending: false,
-        guests: 0,
-        address: "",
-        city: "",
-        state: "",
-        zip: "",
-      }}
-      validationSchema={RSVPValidationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        createRSVP({
-          variables: {
-            name: values.name,
-            email: values.email,
-            attending: values.attending,
-            guests: values.guests,
-            address: values.address,
-            city: values.city,
-            state: values.state,
-            zip: values.zip,
-          },
-        });
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => {
-        <Form>
-          <div>
-            <label htmlFor="name">Name</label>
-            <Field type="text" name="name" id="name" />
-            <ErrorMessage name="name" component="div" className="error" />
-          </div>
-
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field type="text" name="email" id="email" />
-            <ErrorMessage name="email" component="div" />
-          </div>
-
-          <div>
-            <label>Are you attending?</label>
-            <div role="group" aria-labelledby="attending">
-              <label htmlFor="attending">Yes</label>
-              <Field type="radio" name="attending" value="yes" />
-              <label htmlFor="attending">No</label>
-              <Field type="radio" name="attending" value="no" />
-              <ErrorMessage name="name" component="div" />
-            </div>
-            <ErrorMessage name="attending" component="div" className="error" />
-          </div>
-
-          <div>
-            <label htmlFor="guests"> List of Guests</label>
-            <Field type="number" id="guests" name="guests" />
-            <ErrorMessage name="guests" component="div" className="error" />
-          </div>
-
-          <div>
+    <div>
+      <div>hello world</div>
+      <h1>RSVP form</h1>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          attending: false,
+          guests: 0,
+          address: "",
+          city: "",
+          state: "",
+          zip: "",
+          
+        }}
+        validationSchema={RSVPValidationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          createRSVP({
+            variables: {
+              name: values.name,
+              email: values.email,
+              attending: values.attending,
+              guests: values.guests,
+              address: values.address,
+              city: values.city,
+              state: values.state,
+              zip: values.zip,
+            },
+          });
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => {
+          return (
+          <Form>
+            
             <div>
-              <label htmlFor="address"> Street Address</label>
-              <Field type="test" id="address" name="address" />
-              <ErrorMessage name="address" component="div" className="error" />
+              <label htmlFor="name">Name</label>
+              <Field type="text" name="name" id="name" />
+              <ErrorMessage name="name" component="div" className="error" />
             </div>
 
             <div>
-              <label htmlFor="city"> City:</label>
-              <Field type="text" id="city" name="city" />
-              <ErrorMessage name="city" component="div" className="error" />
+              <label htmlFor="email">Email</label>
+              <Field type="text" name="email" id="email" />
+              <ErrorMessage name="email" component="div" />
             </div>
 
             <div>
-              <label htmlFor="State"> State:</label>
-              <Field type="text" id="state" name="state" />
-              <ErrorMessage name="state" component="div" className="error" />
+              <label>Are you attending?</label>
+              <div role="group" aria-labelledby="attending">
+                <label htmlFor="attending">Yes</label>
+                <Field type="radio" name="attending" value="yes" />
+                <label htmlFor="attending">No</label>
+                <Field type="radio" name="attending" value="no" />
+                <ErrorMessage name="name" component="div" />
+              </div>
+              <ErrorMessage
+                name="attending"
+                component="div"
+                className="error"
+              />
             </div>
 
             <div>
-              <label htmlFor="zip"> Zip Code:</label>
-              <Field type="number" id="zip" name="zip" />
-              <ErrorMessage name="zip" component="div" className="error" />
+              <label htmlFor="guests"> List of Guests</label>
+              <Field type="number" id="guests" name="guests" />
+              <ErrorMessage name="guests" component="div" className="error" />
             </div>
-          </div>
-        </Form>;
-      }}
-    </Formik>
+
+            <div>
+              <div>
+                <label htmlFor="address"> Street Address</label>
+                <Field type="test" id="address" name="address" />
+                <ErrorMessage
+                  name="address"
+                  component="div"
+                  className="error"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="city"> City:</label>
+                <Field type="text" id="city" name="city" />
+                <ErrorMessage name="city" component="div" className="error" />
+              </div>
+
+              <div>
+                <label htmlFor="State"> State:</label>
+                <Field type="text" id="state" name="state" />
+                <ErrorMessage name="state" component="div" className="error" />
+              </div>
+
+              <div>
+                <label htmlFor="zip"> Zip Code:</label>
+                <Field type="number" id="zip" name="zip" />
+                <ErrorMessage name="zip" component="div" className="error" />
+              </div>
+
+              <button type="submit " disabled={isSubmitting}>
+                Submit
+              </button>
+            </div>
+          </Form>
+          );
+        }}
+      </Formik>
+    </div>
   );
 };
 
