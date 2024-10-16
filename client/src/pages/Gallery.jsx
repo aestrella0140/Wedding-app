f9e9582d11e39423f2f255e89779e3527773a914
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-10-15 18:20:32 -0700

    css for the array of images might change to one object of image

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -8,66 +8,54 @@ import meAndLove from "../assets/meAndLove.jpeg";
 import chapo from "../assets/chapo1.jpeg";
 
 const images = [
-    {
-        original: us,
-        thumbnail: "us.jpeg",
-        description: "description of image 1",
-    },
-    {
-        original: meAndHer,
-        thumbnail: "meAndHer.jpeg",
-        description: "description of image 2",
-    },
-    {
-        original: chapo,
-        thumbnail: "chapo1.jpeg",
-        description: "description of image 3",
-    },
-    {
-        original: meAndLove,
-        thumbnail: "meAndLove.jpeg",
-        description: "description of image 4",
-    },
-    {
-        original: "url-to-image1.png",
-        thumbnail: "url-to-thumbnail5.png",
-        description: "description of image 5",
-    },
-    {
-        original: "url-to-image1.png",
-        thumbnail: "url-to-thumbnail6.png",
-        description: "description of image 6",
-    },
+  {
+    original: us,
+    thumbnail: us,
+    description: "description of image 1",
+  },
+  {
+    original: meAndHer,
+    thumbnail: meAndHer,
+    description: "description of image 2",
+  },
+  {
+    original: chapo,
+    thumbnail: chapo,
+    description: "description of image 3",
+  },
+  {
+    original: meAndLove,
+    thumbnail: meAndLove,
+    description: "description of image 4",
+  },
 ];
 
 const PhotoGallery = () => {
+  const breakPointColObj = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
 
-    const breakPointColObj = {
-        default: 3,
-        1100: 2,
-        700: 1
-    };
-
-    return (
-        <Masonry
-        breakpointCols={breakPointColObj}
-        className="masonry-grid"
-        columnClassName="masonry-grid-column"
-        >
-            {images.map((image, index) => (
-            <div key={index} className="image">
-            <Gallery
-             items={images}
-             showThumbnails={true}
-             showPlayButton={true}
-             showFullscreenButton={true} 
-             showNav={true}/>
-
-            </div>
-
-            ))}
-        </Masonry>
-    );
+  return (
+    <Masonry
+      breakpointCols={breakPointColObj}
+      className="masonry-grid"
+      columnClassName="masonry-grid_column"
+    >
+      {images.map((image, index) => (
+        <div key={index} className="image-gallery-container">
+          <Gallery
+            items={images}
+            showThumbnails={true}
+            showPlayButton={true}
+            showFullscreenButton={true}
+            showNav={true}
+          />
+        </div>
+      ))}
+    </Masonry>
+  );
 };
 
 export default PhotoGallery;
