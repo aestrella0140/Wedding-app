14c5ef4cc7cb240b8254091b0ec46f98f6c84e27
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-10-29 18:03:33 -0700

    started the google maps api for the event.jsx page to have interactive map

---
+++ client/src/pages/Event.jsx
@@ -0,0 +1,3 @@
+// this page is to show the location of the event and the location of the reception using google maps api
+
+const api = 'AIzaSyBQS3Tx21WxrN7Tuwlk520N7L-cMXPVvRs'
---
+++ client/src/pages/Registry.jsx
@@ -0,0 +1,1 @@
+// this page is for URLs to registries that the owner has created --- later project task
