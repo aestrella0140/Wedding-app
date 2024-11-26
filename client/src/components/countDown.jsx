b97a8444a7d46d924cd8f839c2c7bebfe7630c65
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-11-26 16:25:33 -0700

    finished time countdown for the big day before integration into homepage and other uses

--- client/src/components/countDown.jsx
+++ client/src/components/countDown.jsx
@@ -14,14 +14,22 @@ const countDown = ({ targetDate }) => {
                 setTimeLeft("The big day is here!");
             } else {
                 const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                const hours = Math.floor((distance / (1000 * 60 * 60 * 24)) / (1000 * 60 *60))
+                const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+                const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                setTimeLeft(`${days}d ${hours}h ${minutes}m ${seconds}s`);
             }
-        })
-    })
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [targetDate]);
 
     return (
         <div>
-
+            <p>{timeLeft}</p>
         </div>
     )
 };
+
+export default countDown;
