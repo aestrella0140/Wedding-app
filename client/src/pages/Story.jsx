2b84721abd0286911827e38ff88cc372f39beaf2
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-02-27 17:37:40 -0700

    fixed card layout before grid pattern all cards the same size now

--- client/src/pages/Story.jsx
+++ client/src/pages/Story.jsx
@@ -35,11 +35,14 @@ const Story = () => {
         </ul>
       </nav>
 
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-        <div className="col">
+      <div
+        className="row"
+        style={{ width: 900 }}
+      >
+        <div>
           <div className="card p-3">
+            <h2>How it began</h2>
             <p>
-              <h2>How it began</h2>
               Our journey began long before we even realized it. We first met in
               Pre-K at Killip Elementary and continued into Kindergarten at
               South Beaver Elementary. On the very first day, Tatiana’s dad told
@@ -62,162 +65,164 @@ const Story = () => {
             </p>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p>
               <h2>reconnecting</h2>
-              During those visits, Adrian and Tatiana grew closer, secretly
-              holding hands when no one was looking. One night, while sitting on
-              the couch holding hands, Adrian’s dad woke up and saw us but said
-              nothing until the next day. Leading up to freshman year, we
-              finally started dating—though only for about a month. Adrian,
-              being awkward and not knowing how to talk to Tatiana,
-              unintentionally crashed and burned the relationship by simply
-              being too quiet. Despite this, we remained friends, though not as
-              close during freshman year. By sophomore year, we reconnected and
-              talked as friends again. With both of us busy in sports and clubs,
-              we didn’t hang out often, but it was nice to have each other to
-              talk to. Junior year got even busier—Tatiana took on Starbucks,
-              honor guard, and honors classes, while Adrian was focused on
-              wrestling, recovering from a torn ACL, and training for
-              Greco-Roman wrestling. That summer, Adrian joined Uncle Richard’s
-              Young Lions team, training in Denmark and Germany. We barely saw
-              each other, but toward the end of senior year, our paths crossed
-              again.
-            </p>
+              <p>
+                During those visits, Adrian and Tatiana grew closer, secretly
+                holding hands when no one was looking. One night, while sitting
+                on the couch holding hands, Adrian’s dad woke up and saw us but
+                said nothing until the next day. Leading up to freshman year, we
+                finally started dating—though only for about a month. Adrian,
+                being awkward and not knowing how to talk to Tatiana,
+                unintentionally crashed and burned the relationship by simply
+                being too quiet. Despite this, we remained friends, though not
+                as close during freshman year. By sophomore year, we reconnected
+                and talked as friends again. With both of us busy in sports and
+                clubs, we didn’t hang out often, but it was nice to have each
+                other to talk to. Junior year got even busier—Tatiana took on
+                Starbucks, honor guard, and honors classes, while Adrian was
+                focused on wrestling, recovering from a torn ACL, and training
+                for Greco-Roman wrestling. That summer, Adrian joined Uncle
+                Richard’s Young Lions team, training in Denmark and Germany. We
+                barely saw each other, but toward the end of senior year, our
+                paths crossed again.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p>
               <h2>realigning</h2>
-              With a little help from a mutual friend playing matchmaker, we
-              finally went on our first real date. Castles and Coasters was
-              supposed to be a double date with Adrian’s sister and her
-              boyfriend, but they bailed last minute. Nervous as ever, Adrian
-              begged his sister to still come along. Fortunately, Tatiana and
-              Gabby were already friends, so she tagged along instead. From that
-              day, we were inseparable. We spent every weekend together, going
-              to hookah lounges and hanging out at friends' houses.
-            </p>
+              <p>
+                With a little help from a mutual friend playing matchmaker, we
+                finally went on our first real date. Castles and Coasters was
+                supposed to be a double date with Adrian’s sister and her
+                boyfriend, but they bailed last minute. Nervous as ever, Adrian
+                begged his sister to still come along. Fortunately, Tatiana and
+                Gabby were already friends, so she tagged along instead. From
+                that day, we were inseparable. We spent every weekend together,
+                going to hookah lounges and hanging out at friends' houses.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p>
               <h2>New beginings</h2>
-              As graduation neared, we started planning for the future. Tatiana
-              wanted to join the Air Force, while Adrian was unsure of his next
-              steps. We both started working—Tatiana at Starbucks and Adrian at
-              Gore. When COVID hit, Tatiana joined Gore too, bringing us even
-              closer. Our one-year anniversary came, and we decided to get an
-              Airbnb in Prescott, AZ. The trip was memorable—not just because of
-              our time together, but also because of the odd stares we received
-              from locals. Feeling out of place, we stayed inside, cooking for
-              each other and having deep conversations. On our second-to-last
-              day, we took a trip to Sedona but accidentally drove the back way
-              through Jerome and Cottonwood. The thick fog and steep roads were
-              nerve-wracking, and our brakes overheated, but we made it safely
-              and had an amazing time.
-            </p>
+              <p>
+                As graduation neared, we started planning for the future.
+                Tatiana wanted to join the Air Force, while Adrian was unsure of
+                his next steps. We both started working—Tatiana at Starbucks and
+                Adrian at Gore. When COVID hit, Tatiana joined Gore too,
+                bringing us even closer. Our one-year anniversary came, and we
+                decided to get an Airbnb in Prescott, AZ. The trip was
+                memorable—not just because of our time together, but also
+                because of the odd stares we received from locals. Feeling out
+                of place, we stayed inside, cooking for each other and having
+                deep conversations. On our second-to-last day, we took a trip to
+                Sedona but accidentally drove the back way through Jerome and
+                Cottonwood. The thick fog and steep roads were nerve-wracking,
+                and our brakes overheated, but we made it safely and had an
+                amazing time.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p>
               <h2>Family</h2>
-              During the lockdown, we found comfort in each other and
-              binge-watched countless anime and Studio Ghibli movies—films we
-              still rewatch today. However, this time was also difficult.
-              Adrian’s dad fell seriously ill, and we spent most of our time at
-              his parents’ house helping take care of him. On April 9, 2021,
-              Adrian’s dad passed away, a loss that deeply impacted us all but
-              ultimately brought our families closer.
-            </p>
+              <p>
+                During the lockdown, we found comfort in each other and
+                binge-watched countless anime and Studio Ghibli movies—films we
+                still rewatch today. However, this time was also difficult.
+                Adrian’s dad fell seriously ill, and we spent most of our time
+                at his parents’ house helping take care of him. On April 9,
+                2021, Adrian’s dad passed away, a loss that deeply impacted us
+                all but ultimately brought our families closer.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p>
-              <h2>Hard Times</h2>A few months later, we decided to get a dog
-              together. Bruno, a tiny 4-pound Maltese mix, became our first pet.
-              Unfortunately, he had parvo, and we rushed him to the emergency
-              vet multiple times, doing everything we could to save him. One
-              night, after another vet visit, we were so overwhelmed that we
-              called our moms while pulling out of the clinic’s parking lot.
-              Just as we waited at a green light, we were t-boned—our first car
-              accident, with our moms hearing everything on the phone. Bruno
-              passed away two days later, leaving us heartbroken.
-            </p>
+              <h2>Hard Times</h2>
+              <p>
+                A few months later, we decided to get a dog together. Bruno, a
+                tiny 4-pound Maltese mix, became our first pet. Unfortunately,
+                he had parvo, and we rushed him to the emergency vet multiple
+                times, doing everything we could to save him. One night, after
+                another vet visit, we were so overwhelmed that we called our
+                moms while pulling out of the clinic’s parking lot. Just as we
+                waited at a green light, we were t-boned—our first car accident,
+                with our moms hearing everything on the phone. Bruno passed away
+                two days later, leaving us heartbroken.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p>
               <h2>Vegas!</h2>
-              For our third anniversary, we took our first out-of-state trip
-              together to Las Vegas, staying at The D Resort. We had no luck
-              gambling but loved the live bands on Fremont Street. We explored
-              the Mob Museum and planned a fun night out on our last day but
-              ended up exhausted, watching the SpongeBob movie instead. Still,
-              it was an unforgettable trip.
-            </p>
+              <p>
+                For our third anniversary, we took our first out-of-state trip
+                together to Las Vegas, staying at The D Resort. We had no luck
+                gambling but loved the live bands on Fremont Street. We explored
+                the Mob Museum and planned a fun night out on our last day but
+                ended up exhausted, watching the SpongeBob movie instead. Still,
+                it was an unforgettable trip.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p>
               <h2>The Big Question</h2>
-              By our fourth anniversary, Adrian had big plans. While in Modesto,
-              CA, for family matters, he secretly arranged a surprise picnic at
-              Boise Thompson Ranch, a beautiful tree farm. Adrian’s sister,
-              Gabby, was in on it, secretly recording as Adrian got down on one
-              knee and proposed.
-              <strong>Tatiana said yes!</strong> Her engagement ring, featuring
-              a stunning Tsavorite garnet gem, was the perfect symbol of our
-              journey together.
-            </p>
+              <p>
+                By our fourth anniversary, Adrian had big plans. While in
+                Modesto, CA, for family matters, he secretly arranged a surprise
+                picnic at Boise Thompson Ranch, a beautiful tree farm. Adrian’s
+                sister, Gabby, was in on it, secretly recording as Adrian got
+                down on one knee and proposed.
+                <strong>Tatiana said yes!</strong> Her engagement ring,
+                featuring a stunning Tsavorite garnet gem, was the perfect
+                symbol of our journey together.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p className="card">
               <img src={chunk} alt="french bulldog with a bowtie" />
               <h2>Friends and dog</h2>
-              Shortly after, Adrian took a Full Stack Web Development course
-              through ASU, changing shifts at work, while Tatiana got a new car
-              she absolutely loved. Not long after, we got a French bulldog
-              named Chapo. He is our baby, and although he has a lot of vet
-              bills, he is the love of our lives, and everyone adores him. He is
-              a brat, but he is so loving, always making us laugh with his
-              antics and snorts. We became more involved with our growing friend
-              group and planned our first group trip for Tequila Sunrise in
-              Flagstaff. We all rented an Airbnb and spent the weekend
-              bar-hopping, with Collins Bar becoming our favorite spot. Walking
-              back home drunk was an adventure, but it was one of the best times
-              we’d had.
-            </p>
+              <p>
+                Shortly after, Adrian took a Full Stack Web Development course
+                through ASU, changing shifts at work, while Tatiana got a new
+                car she absolutely loved. Not long after, we got a French
+                bulldog named Chapo. He is our baby, and although he has a lot
+                of vet bills, he is the love of our lives, and everyone adores
+                him. He is a brat, but he is so loving, always making us laugh
+                with his antics and snorts. We became more involved with our
+                growing friend group and planned our first group trip for
+                Tequila Sunrise in Flagstaff. We all rented an Airbnb and spent
+                the weekend bar-hopping, with Collins Bar becoming our favorite
+                spot. Walking back home drunk was an adventure, but it was one
+                of the best times we’d had.
+              </p>
             </div>
           </div>
 
-          <div className="col">
+          <div>
             <div className="card p-3">
-            <p >
               <h2>present</h2>
-              For our fifth anniversary, we planned a trip to Tucson, exploring
-              downtown, visiting quirky shops, and simply enjoying each other’s
-              company. As we look back on our journey—from childhood friends to
-              high school sweethearts, from struggles to celebrations—we know
-              that every moment has led us to where we are today. And we
-              wouldn’t change a single thing.
-            </p>
+              <p>
+                For our fifth anniversary, we planned a trip to Tucson,
+                exploring downtown, visiting quirky shops, and simply enjoying
+                each other’s company. As we look back on our journey—from
+                childhood friends to high school sweethearts, from struggles to
+                celebrations—we know that every moment has led us to where we
+                are today. And we wouldn’t change a single thing.
+              </p>
             </div>
           </div>
         </div>
