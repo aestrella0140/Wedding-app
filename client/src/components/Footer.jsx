65c2204f7dee184655319fed58d227e36cf26a29
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-03-11 17:18:46 -0700

    styled footer with anchors to Resources and contacts with copyright stamp

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,25 +2,26 @@ import React from "react";
 
 const footer = () => {
     return (
-        <footer className="container py-5">
+        <footer className="footer container py-5">
       <div className="row">
-        <div className="col-12 col-md">
-          <small className="d-block mb-3 text-muted">&copy; 2025</small>
-        </div>
-        <div className="col-6 col-md">
-          <h5>Section</h5>
+        <div className="col-6">
+          <h5>Contacts</h5>
           <ul className="list-unstyled text-small">
-            <li><a className="text-muted" href="#">Cool stuff</a></li>
-            <li><a className="text-muted" href="#">Random feature</a></li>
-            <li><a className="text-muted" href="#">Team feature</a></li>
+            <li><a className="text-muted" href="mailto: astarfirstborn@hotmail.com">Email: astarfirstborn@hotmail.com</a></li>
+            <li><a className="text-muted" href="#">Person Phone: 623-755-3094</a></li>
+            <li><a className="text-muted" href="#"></a></li>
           </ul>
         </div>
         <div className="col-6 col-md">
           <h5>Resources</h5>
           <ul className="list-unstyled text-small">
-            <li><a className="text-muted" href="#">Resource</a></li>
+            <li><a className="text-muted" href="https://github.com/aestrella0140/Wedding-app">Repository</a></li>
             <li><a className="text-muted" href="#">Another resource</a></li>
           </ul>
+        </div>
+        <div className="col-12 col-md">
+          <h5>CopyRight</h5>
+          <small className="text-muted">&copy; 2025 <a className="text-muted" href="https://github.com/aestrella0140" target="_blank" rel="noopener noreferrer">aestrella0140</a> : All rights reserved</small>
         </div>
       </div>
     </footer>
