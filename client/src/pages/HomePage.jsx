72515697d9765d205a560abdeaac972e77bf3380
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-04-22 17:52:45 -0700

    aside content on homepage for quick link to directions already styled

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,10 +6,8 @@ import styles from "./homePage.module.css";
 import CountDown from "../components/countDown";
 
 const HomePage = () => {
-
   return (
     <main className={styles.home}>
-      
       <img
         src={flowerTopper}
         alt="eucalyptus drawing"
@@ -41,12 +39,28 @@ const HomePage = () => {
         <p>
           We're so excited to celebrate our special day with all of you. Here,
           you'll find everything you need to know about our big day, from event
-          details to RSVP and more. Thank you for being part of our journey — we
+          details to RSVP and more! Thank you for being part of our journey — we
           can't wait to see you on February 23, 2026!
         </p>
-        <section className={`${styles.imageSection} home-image my-4`}>
-          <img src={usThumbnail} alt="Adrian and Tatiana.." className="img" />
-        </section>
+
+        <aside className="aside">
+          <div className="aside-p">
+            <h4>RECEPTION</h4>
+            <p>
+              <a 
+                href="https://www.google.com/maps/dir/?api=1&destination=246727+N+181st+Dr,Surprise,AZ"
+                target="_blank"
+                rel="noopener noreferrer"
+                className=""
+              >
+                The Ranch
+              </a>
+            </p>
+          </div>
+          <section className={`${styles.imageSection} home-image my-4`}>
+            <img src={usThumbnail} alt="Adrian and Tatiana.." className="img" />
+          </section>
+        </aside>
       </div>
     </main>
   );
