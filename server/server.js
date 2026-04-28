078b28c001f5fbf79d07349029f7fb7f9ab7d5cf
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2026-04-28 16:42:56 -0700

    adding cors to backend so backend can communicate to front end

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,8 +17,14 @@ const server = new ApolloServer({
     resolvers,
 });
 
+
 const startApolloServer = async () => {
     await server.start();
+
+    app.use(cors({
+        origin: "https://wedding-frontend.netlify.app/",
+        credentials: true
+    }));
 
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
