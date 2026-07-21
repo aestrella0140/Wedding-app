7519deeadcc0f1f8c3dbff4e67437b5e710e1169
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2026-07-20 17:14:22 -0700

    styled button on registry page for honeyfund link

--- client/src/pages/Registry.jsx
+++ client/src/pages/Registry.jsx
@@ -25,24 +25,31 @@ const Registry = () => {
         </ul>
       </nav>
       <div className="registry-hero">
-        <h2>Our Registry</h2>
+        <h2>Honeyfund</h2>
 
-          <div className="divider">
-            <span>✿</span> 
-          </div>
-
-        
-            <p className="lead">
-              We're so grateful for your love and support, If you'd like to
-              bless us with a gift, You can find our registries here:
-            </p>
-
-            <ul className="list-unstyled">
-              <li>
-                <a href="https://www.honeyfund.com/site/TatianaandAdrian2027">Honeyfund</a>
-              </li>
-            </ul>
+        <div className="divider">
+          <span>✿</span>
         </div>
+
+        <div>
+          <p className="lead">
+            We're so grateful for your love and support, If you'd like to bless
+            us with a gift, You can find our registries here:
+          </p>
+          <a
+            href="https://www.honeyfund.com/site/TatianaandAdrian2027"
+            target="_blank"
+            rel="noreferrer"
+            className="honeyfund-btn"
+          >
+            <span className="shadow"></span>
+              <span className="edge"></span>
+                <span className="front">
+                  <span>Honeyfund</span>
+                </span>
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
